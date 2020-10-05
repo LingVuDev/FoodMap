d804b12a350cc155fc37d5fd4bdfadb5e8369b0d
Manh Ling Vu <vumanhling@icloud.com>
2020-10-05 20:22:40 +0200

    FE: Add restaurants context

--- foodmap-client/src/App.jsx
+++ foodmap-client/src/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 import { DetailPage } from './routes/DetailPage';
 import { Home } from './routes/Home';
 import { UpdatePage } from './routes/UpdatePage';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/restaurants/:id/update" component={UpdatePage}></Route>
-                <Route exact path="/restaurants/:id" component={DetailPage}></Route>
-            </Switch>
-        </BrowserRouter>
+        <RestaurantsContextProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route exact path="/restaurants/:id/update" component={UpdatePage}></Route>
+                    <Route exact path="/restaurants/:id" component={DetailPage}></Route>
+                </Switch>
+            </BrowserRouter>
+        </RestaurantsContextProvider>
+        
     );
 }
 
